40f0526414edc1832147d7d2f4a73056cc1ec92d
Lawrence Yirenkyi-Boafo <lawrencekyb@gmail.com>
2023-12-17 22:42:18 +0000

    progress..socket.io implementation

--- src/controllers/socket.ts
+++ src/controllers/socket.ts
@@ -1,56 +1,76 @@
-import { Server as SocketIOServer, Socket } from 'socket.io';
-import Logger from '../lib/logger';
-
-// Define a type for online users
-interface OnlineUsers {
-  [userId: string]: Socket;
-}
+import { Server as SocketIOServer, Socket } from "socket.io";
+import Logger from "../lib/logger";
 
 export default class SocketManager {
   private io: SocketIOServer;
-  private onlineUsers: OnlineUsers = {};
+  private onlineUsers: Map<string, string> = new Map<string, string>();
 
   constructor(io: SocketIOServer) {
     this.io = io;
     this.setupSocketEvents();
-    console.log('Socket Manager Initialized');
+    console.log("Socket Manager Initialized");
   }
 
   private setupSocketEvents(): void {
-    // Handle connection event
-    this.io.on('connection', (socket: Socket) => {
-        socket.emit('test', 'Hello Testing event!');
-
+    this.io.on("connection", (socket: Socket) => {
       this.handleUserConnection(socket);
 
-      // Handle disconnection event
-      socket.on('disconnect', () => {
+      socket.on("disconnect", () => {
         this.handleUserDisconnection(socket);
       });
 
-  
+      socket.on("privateMessage", (data) => {
+        this.handlePrivateMessage(socket, data);
+      });
     });
   }
 
   handleUserConnection(socket: Socket): void {
     Logger.info(`User connected: ${socket.id}`);
+    socket.on("addUser", (userID) => {
+      const {userId} = userID;
+      Logger.info(`Received addUser event for userID: ${userId}`);
+      const isExisting = this.onlineUsers.has(userId);
+      console.log(`User ${userId} exists: ${isExisting}`);
+      
+      if (!isExisting) {
+        this.onlineUsers.set(userId, socket.id);
+        console.log(`Added user ${userId} to onlineUsers map`);
+      }
+      
+      console.log(this.onlineUsers);
+    });
   }
+  
 
+  handlePrivateMessage(
+    socket: Socket,
+    data: { userId: string; message: string }
+  ): void {
+    const { userId, message } = data;
+    const targetSocketId = this.onlineUsers.get(userId) as string;
+    console.log(this.onlineUsers.get(userId))
+    socket.to(targetSocketId).emit("privateMessage", {
+      senderSocketId: socket.id,
+      message: message,
+    });
+
+    socket.emit("privateMessage", {
+      receiverSocketId: targetSocketId,
+      message: message,
+    });
+  }
   handleUserDisconnection(socket: Socket): void {
     Logger.info(`User disconnected: ${socket.id}`);
-    // Handle user disconnection, remove from onlineUsers if needed
-    // const userId = Object.keys(this.onlineUsers).find(
-    //   (key) => this.onlineUsers[key].id === socket.id
-    // );
-    // if (userId) {
-    //   delete this.onlineUsers[userId];
-    // }
+    this.onlineUsers.forEach((value, key) => {
+      if (value === socket.id) {
+        this.onlineUsers.delete(key);
+      }
+    })
+    console.log(this.onlineUsers)
   }
-
-
 
   getIo(): SocketIOServer {
     return this.io;
   }
 }
-
